e15b79ad4fc97b14c98ec4a7271ab88ad21b5118
uchenick55 <uchenick55@yandex.ru>
2023-05-23 06:18:25 +0300

    my first commit

---
+++ src/types/commonTypes.ts
@@ -0,0 +1,10 @@
+import {Action} from "redux";
+import {ThunkAction} from "redux-thunk";
+import {GlobalStateType} from "../redux/store-redux";
+
+export type ComThunkTp<A extends Action> = ThunkAction<
+    void,    // санка ничего не возвращает
+    GlobalStateType,    // глобальный стейт из redux
+    unknown,    // нет доп параметров
+    A // все типы ActionCreator
+    >
