84bbdaa1c4ba101aef4271c48e0231fc2df173e6
uchenick55 <uchenick55@yandex.ru>
2023-05-25 21:30:23 +0300

    надпись - ничего не найдено, если лист данных пустой

--- src/components/Github/RepositoriesList/GitHubCOM.tsx
+++ src/components/Github/RepositoriesList/GitHubCOM.tsx
@@ -6,6 +6,8 @@ import RenderSearchField from "./RenderSearchField";
 import Pagination from "./Pagination";
 import {RepositoriesDataType} from "../../../common/types/commonTypes";
 import Preloader from "../../../common/Preloader/Preloader";
+import notFound2 from "../../../assets/svg/404.svg"
+import dustBin from "../../../assets/svg/dust-bin2.svg"
 
 export type GitHubCOMType = {
     MyRepositoriesData: Array<RepositoriesDataType> , // автополучение типа входящих данных моих репозиториев
@@ -27,7 +29,16 @@ const GitHubCOM: React.FC<GitHubCOMType> = (
 
             <RenderSearchField setSearchQuery={setSearchQuery} SearchQuery={SearchQuery}/> {/*отрисовка поля поиска */}
 
-            {RepositoriesData.length>0 && <RenderRepositories RepositoriesData={RepositoriesData} PaginationData={PaginationData}/>}
+            {RepositoriesData.length>0
+                ? <RenderRepositories RepositoriesData={RepositoriesData} PaginationData={PaginationData}/>
+                : <div>
+                    {!IsFetching && <div>
+                        <div className={s.NotFoundText}>Ничего не найдено</div>
+                        <img className={s.NotFoundImg} src={notFound2} alt=""/>
+                    </div>}
+
+                </div>
+            }
             {/*отрисовка списка репозиториев*/}
 
             <Pagination totalRepositoriesCount={RepositoriesData.length} pageSize={PaginationData.pageSize}
