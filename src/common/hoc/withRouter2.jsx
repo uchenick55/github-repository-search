8cd892d628522131c112e055627f8ef2e28bc2e7
uchenick55 <uchenick55@yandex.ru>
2023-05-26 09:19:06 +0300

    withRouter2, compose - добавил данные userId из URL браузера

---
+++ src/common/hoc/withRouter2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import {connect} from "react-redux";
+
+let withRouter2 = (Children) => {
+    const WithRouter2withProps = (props) => {
+        const aaa = {params: useParams()}
+        const userId=aaa.params['*'];
+        return <Children {...props} userId={userId}/>
+    }
+    return connect(null, null)(WithRouter2withProps)
+    // доп данные с connect пока нулевые, если понадобятся, сможем добавить
+}
+export default withRouter2
--- src/components/Github/Card/CardContainer.tsx
+++ src/components/Github/Card/CardContainer.tsx
@@ -2,8 +2,11 @@ import {GlobalStateType} from "../../../redux/store-redux";
 import {connect} from "react-redux";
 import React from "react";
 import CardCommon from "./CardCommon";
+import {compose} from "redux";
+import withRouter2 from "../../../common/hoc/withRouter2";
 
-const CardContainer:React.FC<mapStateToPropsType & mapDispatchToPropsType> = ({CardData}) => {
+const CardContainer:React.FC<mapStateToPropsType & mapDispatchToPropsType & OwnPropsType> = (
+    {CardData, userId}) => {
     return <div>
         <CardCommon CardData={CardData}/>
     </div>
@@ -14,12 +17,20 @@ const mapStateToProps = (state: GlobalStateType) => {
         CardData: state.ghCard.CardData
     }
 }
+type OwnPropsType = {
+    userId: number // id пользователя
+}
 type mapStateToPropsType = ReturnType<typeof mapStateToProps>
 
 type mapDispatchToPropsType = {
 }
-export default connect<mapStateToPropsType,
-    mapDispatchToPropsType,
-    unknown,
-    GlobalStateType>( mapStateToProps, {
-} )( CardContainer );
+export default compose<React.ComponentType>(
+
+    connect<mapStateToPropsType, // тип mapStateToProps
+        mapDispatchToPropsType, // тип mapDispatchToProps
+        OwnPropsType, // тип входящих пропсов от родителя
+        GlobalStateType // глобальный стейт из стора
+        >( mapStateToProps, {
+    } ),
+    withRouter2,// получить данные ID из URL браузера и добавить в пропсы
+)( CardContainer )
